96232aa19421d5d0178edff24ed6f90332410075
Documenter.jl <documenter@juliadocs.github.io>
2023-12-24 21:41:34 +0000

    build based on 0f3bb11

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TemperatureSensors","category":"page"},{"location":"#TemperatureSensors","page":"Home","title":"TemperatureSensors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TemperatureSensors.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TemperatureSensors]","category":"page"}]
+}
