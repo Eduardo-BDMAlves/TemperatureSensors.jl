1d148fcf57bda98005e2e7c39c1b05ea60172e56
Eduardo-BDMAlves <eduardo.alves@polo.ufsc.br>
2024-01-21 15:35:18 -0300

    Corrected problems with test on Unitful integration.
    Seems to be related to problem with macros and tests suit. Avoid macros in the future.

--- test/UnitfulTests.jl
+++ test/UnitfulTests.jl
@@ -1,13 +1,13 @@
 @safetestset "Temperature Conversions" begin
     using TemperatureSensors
     using Unitful
+    using Unitful: °C, K, Ω, kΩ, mΩ
 
     sensor = ITS90PT100(-2.0386711E-2, 3.3068936E-3, -1.9700442E-02, 100.0121)
 
-    transition_temperature = temperature(sensor.ITS90_transition_resistance, sensor)u"K" |>
-                             u"°C"
+    transition_temperature = temperature(sensor.ITS90_transition_resistance, sensor)K |> °C
 
-    transition_resistance = resistance(transition_temperature, sensor, u"kΩ")
+    transition_resistance = resistance(transition_temperature, sensor, kΩ)
 
     transition_resistance_kΩ_striped = ustrip(transition_resistance)
 
@@ -18,10 +18,10 @@
 
     @test transition_resistance_kΩ_striped ≈ sensor.ITS90_transition_resistance / 1000
 
-    Rs = [100u"Ω", 0.101u"kΩ", 110u"Ω"]
+    Rs = [100Ω, 0.101kΩ, 110Ω]
 
     Ts = temperature(Rs, sensor)
     Rs_from_Ts = resistance(Ts, sensor)
 
-    @test Rs ≈ Rs_from_Ts
+    @test Rs≈Rs_from_Ts atol=0.1mΩ
 end
